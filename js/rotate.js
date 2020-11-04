28f2ddab13ec7afdb0ea0de484733fae25112c44
IfElseSwitch <dygit4lis@gmail.com>
2020-11-04 01:14:30 +0100

    attendre svg load

--- js/rotate.js
+++ js/rotate.js
@@ -4,22 +4,22 @@ window.addEventListener('load', _ => {
   const outerBtn = document.getElementById('outer-btn');
   const middleBtn = document.getElementById('middle-btn');
   const innerBtn = document.getElementById('inner-btn');
-
-  rotate(0, +180)();
-  rotate(1, +180)();
-  rotate(1, +180)();
-  outerBtn.addEventListener('click', rotate(0, +90));
-  middleBtn.addEventListener('click', rotate(1, +90));
-  innerBtn.addEventListener('click', rotate(2, +90));
-
-  function rotate(rotIdx, angle) {
-    return _ => {
-      console.log(`rotate ${rotIdx} by ${angle}`);
-      const rot = (rotation[rotIdx] + angle) % 360;
-      rotation[rotIdx] = rot;
-      const svgObject = document.getElementById('svg-object').contentDocument;
-      const toRotate = svgObject.getElementById(ids[rotIdx]);
-      toRotate.setAttribute("transform", `rotate(${rot}, 50.27036,50.27036)`)
+  const svgObject = document.getElementById('svg-object');
+  svgObject.addEventListener('load', _ => {
+    rotate(0, +180)();
+    rotate(1, +180)();
+    rotate(1, +180)();
+    outerBtn.addEventListener('click', rotate(0, +90));
+    middleBtn.addEventListener('click', rotate(1, +90));
+    innerBtn.addEventListener('click', rotate(2, +90));
+    function rotate(rotIdx, angle) {
+      return _ => {
+        console.log(`rotate ${rotIdx} by ${angle}`);
+        const rot = (rotation[rotIdx] + angle) % 360;
+        rotation[rotIdx] = rot;
+        const toRotate = svgObject.contentDocument.getElementById(ids[rotIdx]);
+        toRotate.setAttribute("transform", `rotate(${rot}, 50.27036,50.27036)`)
+      }
     }
-  }
+  });
 });
