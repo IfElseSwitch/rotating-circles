0992581d1d8a6049105757ce14d51b64cf2f7db5
IfElseSwitch <dygit4lis@gmail.com>
2020-11-04 01:05:04 +0100

    update

--- js/rotate.js
+++ js/rotate.js
@@ -12,8 +12,8 @@ window.addEventListener('load', _ => {
     return _ => {
       const rot = (rotation[rotIdx] + angle) % 360;
       rotation[rotIdx] = rot;
-      const svgObject = document.getElementById('svg-object');
-      const toRotate = document.getElementById(ids[rotIdx]);
+      const svgObject = document.getElementById('svg-object').documentContent;
+      const toRotate = svgObject.getElementById(ids[rotIdx]);
       toRotate.setAttribute("transform", `rotate(${rot}, 50.27036,50.27036)`)
     }
   }
